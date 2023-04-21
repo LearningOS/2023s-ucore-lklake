31d27f9bf4573a032b823902d1025f98bf4d4de2
lklake <yuanchaoxun21@mails.ucas.ac.cn>
2023-04-21 16:19:38 +0800

    merge ch4

--- os/syscall.c
+++ os/syscall.c
@@ -114,6 +114,135 @@ uint64 sys_sbrk(int n)
         return addr;
 }
 
+
+
+// TODO: add support for mmap and munmap syscall.
+// hint: read through docstrings in vm.c. Watching CH4 video may also help.
+// Note the return value and PTE flags (especially U,X,W,R)
+/*
+* LAB1: you may need to define sys_task_info here
+*/
+
+// this syscall handles va unmap and pa free when map failed
+uint64 sys_mmap(uint64 start, uint64 len, int port, int flag, int fd){
+
+	// fall early
+
+	if((port & ~0x7) != 0){
+		errorf("mmap:port contain dirty bits");
+		return 0-1;
+	}
+	if((port & 0x7) == 0 ){
+		errorf("mmap:request non R or W or X memory is meaningless");
+		return -1;
+	}
+	if(!PGALIGNED(start)){
+		errorf("mmap:start address is not page aligned");
+		return -1;
+	}
+	if(len == 0){
+		return 0;
+	}
+	if(len > 1024 * 1024 * 1024){
+		errorf("mmap:too large");
+		return -1;
+	}
+
+	// do the job
+
+	int perm = PTE_U;
+	if(port & 0b1u ){
+		perm |= PTE_R;
+	}
+	if(port & 0b10u ){
+		perm |= PTE_W;
+	}
+	if(port & 0b100u ){
+		perm |= PTE_X;
+	}
+	perm &= (0b11110u);
+
+    struct proc *p = curr_proc();
+
+	int num = PGROUNDUP(len) / PAGE_SIZE;
+	int mapped_pages = 0;
+
+	uint64 pa = 0;
+	uint64 va = start;
+
+	while(mapped_pages < num){
+		pa = (uint64) kalloc();
+		if(pa == 0){
+			// no p memory,unmap and free mapped page ,then fail;
+			uvmunmap(p->pagetable,start,mapped_pages,1);
+			errorf("mmap:no enough physical mem");
+			return -1;
+		}
+		// map phical memory page to user virtual memory space
+		if(mappages(p->pagetable, va, PAGE_SIZE, pa, perm) == -1){
+		    //unmap,free and fail;
+		    uvmunmap(p->pagetable,start,mapped_pages,1);
+
+			// this pa is successfully alloced but not mapped,thus not freed by uvmunmap. 
+			kfree((void *)pa);
+			errorf("mmap:map va to pa failed,possibly already mapped");
+			return -1;
+	    }
+		errorf("success:mmaped:va:0x%x pa:0x%x",va,pa);
+		mapped_pages++;
+		va += PAGE_SIZE;
+	}
+
+	return 0;
+}
+
+// this syscall "handles" va remap and pa realloc when unmap failed
+uint64 sys_munmap(uint64 start, uint64 len){
+
+	// fall early
+	if(!PGALIGNED(start)){
+		errorf("munmap:start address is not page aligned");
+		return -1;
+	}
+	if(len == 0){
+		return 0;
+	}
+	if(!PGALIGNED(len)){
+		// not specified, choose to up align len
+	}
+
+	struct proc *p = curr_proc();
+
+	int num = PGROUNDUP(len) / PAGE_SIZE;
+	uint64 va = start;
+	
+	while(num > 0){
+		// useraddr return 0 when no map, but what if the pa been mapped is 0...
+		if(useraddr(p->pagetable,va) == 0){
+		    errorf("munmap:va range contains page that not mapped to pm");
+			return -1;
+	    }
+		va += PAGE_SIZE;
+		num--;
+	}
+	// unmap and free at end, so as to "handle" va unmap and pa free when map failed
+	uvmunmap(p->pagetable,start,PGROUNDUP(len),1);
+	return 0;
+}
+
+uint64 sys_task_info(TaskInfo* info){
+	TaskInfo k_info;
+	struct proc *p = curr_proc();
+	uint64 current_time_ms = get_cycle() / CPU_FREQ * 1000 + (get_cycle() % CPU_FREQ) * 1000 / CPU_FREQ;
+
+	k_info.status = Running;
+	k_info.time = (int)(current_time_ms - p->time);
+	memmove(k_info.syscall_times,p->syscall_times,sizeof(info->syscall_times));
+	copyout(p->pagetable,(uint64)info,(char *)&k_info,sizeof(TaskInfo));
+	return 0;
+}
+
+
 extern char trap_page[];
 
 void syscall()

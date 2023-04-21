13bae12be7c77f68195f8058516a7ac89d456698
lklake <yuanchaoxun21@mails.ucas.ac.cn>
2023-04-21 23:20:49 +0800

    from ch4

--- os/proc.h
+++ os/proc.h
@@ -7,7 +7,7 @@
 
 #define NPROC (512)
 #define FD_BUFFER_SIZE (16)
-
+#define MAX_SYSCALL_NUM 500
 struct file;
 
 // Saved registers for kernel context switches.
@@ -47,7 +47,21 @@ struct proc {
 	struct file *files[FD_BUFFER_SIZE];
 	uint64 program_brk;
 	uint64 heap_bottom;
+	unsigned int syscall_times[MAX_SYSCALL_NUM];
+	int time;
 };
+typedef enum {
+	UnInit,
+	Ready,
+	Running,
+	Exited,
+} TaskStatus;
+
+typedef struct {
+	TaskStatus status;
+	unsigned int syscall_times[MAX_SYSCALL_NUM];
+	int time;
+} TaskInfo;
 
 int cpuid();
 struct proc *curr_proc();

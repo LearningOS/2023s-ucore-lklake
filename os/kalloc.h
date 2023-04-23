88bf6604a9d3935afe4bcd194123402b1df0b532
lklake <yuanchaoxun21@mails.ucas.ac.cn>
2023-04-23 17:48:41 +0800

    introduce frame reference count support for COW

--- os/kalloc.h
+++ os/kalloc.h
@@ -1,8 +1,14 @@
 #ifndef KALLOC_H
 #define KALLOC_H
+#include "riscv.h"
+struct page{
+	int ref;
+};
 
 void *kalloc();
 void kfree(void *);
 void kinit();
-
+int get_page_ref(uint64 pa);
+int get_page(uint64 pa);
+int put_page(uint64 pa);
 #endif // KALLOC_H
